ecb3d04715c65563e4ebfed594d637c5dc88515f
Adrian <131692907+Orphevs@users.noreply.github.com>
2025-04-22 02:09:47 +0900

    Add img thru cloud

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   return (
     <div>
       <h1>Hello, World!!</h1>
+      <img
+        src="https://res.cloudinary.com/YOUR_CLOUD_NAME/image/upload/v1710000000/cat.jpg"
+        alt="dgg7bpdyr"
+        style={{ maxWidth: '300px', marginTop: '20px' }}
+      />
     </div>
   );
 }
